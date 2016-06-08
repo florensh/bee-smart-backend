ca8a083ae459d52be741a183f1a7dcd54dfc93c4
florensh <florensh@gmx.de>
2016-06-08 17:00:08 +0200

    test

--- src/main/java/com/fh/lernkarten/domain/studySession/StudySession.java
+++ src/main/java/com/fh/lernkarten/domain/studySession/StudySession.java
@@ -19,8 +19,25 @@ public class StudySession {
 
 	private Set<Card> knownCards = new HashSet<Card>();
 	private Set<Card> unknownCards = new HashSet<Card>();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Deck getDeck() {
+		return deck;
+	}
+
+	public void setDeck(Deck deck) {
+		this.deck = deck;
+	}
+
 	private Date date;
-	
+
 	@DBRef
 	private Deck deck;
 
